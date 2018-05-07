e1d9bb30fd1b918270607ea45f527ee06bb54a41
numb327 <305985443@qq.com>
2018-05-08 00:08:34 +0800

    jwtauth

---
+++ Models/JwtSettings.cs
@@ -0,0 +1,11 @@
+namespace JwtAuthSample
+{
+    public class JwtSettings
+    {
+        public string Issuer{get; set;}
+
+        public string Audience{get; set;}
+
+        public string SecretKey{get; set;}
+    }
+}
